b646fbce6f3842f6edb40cd08133cbd3f816bacb
David Meda <David-Gabriel.Meda@endava.com>
2022-06-24 12:33:11 +0300

    Added GetBid service

---
+++ Auction_Project/Services/BidServices.cs
@@ -0,0 +1,27 @@
+﻿using Auction_Project.DataBase;
+using Auction_Project.Models;
+
+namespace Auction_Project.Services;
+
+public class BidServices
+{
+    private readonly AppDbContext _context;
+
+    public BidServices(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<Bid> GetBid()
+    {
+
+        if (_context.Bids == null)
+            return null;
+        return _context.Bids.ToList();
+    
+    }
+
+
+}
+
+
