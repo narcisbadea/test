c6fd2b8130c2686fd5c74920f914d0d3838cae85
David Meda <David-Gabriel.Meda@endava.com>
2022-06-24 15:27:50 +0300

    Get and Post

---
+++ Auction_Project/Controllers/BidsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Auction_Project.DataBase;
+using Auction_Project.Services;
+using Auction_Project.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Auction_Project.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BidsController : ControllerBase
+    {
+
+        private readonly BidServices _bidServices;
+
+        public BidsController(BidServices bidServices)
+        {
+            _bidServices = bidServices;
+        }
+
+
+        // GET: api/<BidsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bid>>> Get()
+        {
+            var bids = await _bidServices.GetBids();
+            if (bids.Count == 0)
+                return NotFound("List is empty");
+            return bids;
+        }
+         
+        // GET api/<BidsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bid>> GetById(int id)
+        {
+            var bid = await _bidServices.GetBid(id);
+
+            if (bid != null)
+                return Ok(bid);
+            else
+                return NotFound("Bid not found");
+        }
+
+        // POST api/<BidsController>
+        [HttpPost]
+        public async Task<ActionResult<Bid>> Post(Bid bid)
+        {
+            _bidServices.PostBid(bid);
+            return CreatedAtAction(nameof(Get), new { id = bid.Id }, bid);
+        }
+
+        // PUT api/<BidsController>/5
+        [HttpPut("{id}")]
+        public void Put()
+        {
+        }
+
+        // DELETE api/<BidsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Auction_Project/Services/BidServices.cs
+++ Auction_Project/Services/BidServices.cs
@@ -1,5 +1,13 @@
 ﻿using Auction_Project.DataBase;
 using Auction_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace Auction_Project.Services;
 
@@ -12,16 +20,37 @@ public class BidServices
         _context = context;
     }
 
-    public List<Bid> GetBid()
+    public async Task<List<Bid>> GetBids()
     {
-
-        if (_context.Bids == null)
-            return null;
-        return _context.Bids.ToList();
-    
+        var result = await _context.Bids.ToListAsync();
+        return result; 
     }
 
+    public async Task<Bid> GetBid(int id)
+    {
+        var BidList = await _context.Bids.ToListAsync();
+        var result = BidList.Find(r => r.Id == id); 
+        return result;
+    }
 
+    public async void PostBid(Bid toPost)
+    {
+
+        _context.Bids.Add(toPost);
+        try
+        {
+            _context.SaveChanges();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Kek "+e);
+        }
+    }
+      
+   /* public Bid GetBid(int id)
+    {
+
+    }*/
 }
 
 
