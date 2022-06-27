e862ce11f70e407c20cc06d440d599ac4e08be35
Cristi Păscuț <107857058+Cristi2022@users.noreply.github.com>
2022-06-27 11:17:26 +0300

    created Item

--- Auction_Project/Models/Item.cs
+++ Auction_Project/Models/Item.cs
@@ -2,12 +2,23 @@
 {
     public class Item : Entity
     {
+        private string m_desc = string.Empty;
+        private decimal m_price=0;
+        private string m_ImageAddress = string.Empty;   
 
-        public string? Desc { get; set; }
 
-        public decimal? Price { get; set; }
+        public string Desc { 
+            get { return m_desc; }
+            set { m_desc = value; } }
 
-        public string? ImagesAddress { get; set; }
+        public decimal Price {
+            get { return m_price; } 
+            set { m_price = value; } }
+
+        public string ImagesAddress {
+            get { return m_ImageAddress;}
+            set { m_ImageAddress = value; }
+        }
 
     }
 
